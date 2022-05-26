e1b399d3b3e5931a314bff94b377ffe02cefa3fd
toilathinhh <86840753+toilathinhh@users.noreply.github.com>
2022-05-26 13:30:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 const CONFIG = {
-    introTitle: 'Thư à!',
-    introDesc: `Em ơi anh bảo em này
-    Không yêu anh sớm có ngày mất luôn.`,
-    btnIntro: 'ô kê a luong',
-    title: 'Phải chăng Thư đã bị khùm 😙',
-    desc: 'Phải chăng Thư bị đin ',
-    btnYes: 'đúng là như z',
-    btnNo: 'có cc nè :P',
-    question:'Trên thế giới hơn 7 tỉ người mà sao u bị khùm ???',
-    btnReply: 'Gửi cho Thịnh đzai',
-    reply: 'tại u lây cho toi',
-    mess: 'Anh biết mà 🥰. Bị khùm lâu năm mà giấu',
-    messDesc: 'Tối nay 12h đi ngủ nka em pé.',
-    btnAccept: 'dzạaaaaaaaa',
-    messLink: 'http://facebook.com/nqt106/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com/nqt106' 
 }
